2495d488a5f57636d180ae3a062068f2f692f717
wqyjason <qingyuw2@illinois.edu>
2019-02-20 17:43:08 -0600

    causal ordering test

--- chat.py
+++ chat.py
@@ -156,8 +156,6 @@ def handler(c, a):
             # B-multicast to any other nodes
             sendMsg(sender_stamp, msg, sender_index)
 
-        print("everytime")
-
         # wait until condition meets for hold-back
         # while True:
         if lessThan(sender_stamp, timestamp, sender_index):
