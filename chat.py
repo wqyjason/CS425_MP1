181dda77cce820fc8121dd17445dc6fc496aab9e
wqyjason <qingyuw2@illinois.edu>
2019-02-20 17:08:53 -0600

    all finished

--- chat.py
+++ chat.py
@@ -3,6 +3,7 @@ import threading
 import sys
 import time
 import argparse
+import pickle
 from random import randint
 from threading import Lock
 
@@ -10,8 +11,6 @@ from threading import Lock
 # server-client is build based on TCP and it naturally formed a all-to-all scheme and when
 # any node failed, it will send EOF to all other nodes in the chat group, which every nodes
 # will detect its failure. 
-
-# TO DO !important: implement a reliable multicast and a causal ordering
 
 
 # hard-coded IP addresses for VM
@@ -36,8 +35,48 @@ received = []
 # dictionary for host names
 hostName = {}
 
-# lock for handle received message
-mutex = Lock()
+# hold-back queue for causal ordering
+holdBack = []
+
+
+# timestamp for current process
+timestamp = []
+
+
+
+# lock for handling received message
+mutex_r = Lock()
+
+# lock for hanlding hold-back queue
+mutex_h = Lock()
+
+# lock for handling timestamp
+mutex_t - Lock()
+
+
+# helper to get process number
+def getN():
+    host = socket.gethostname()
+    ret = host.split()[3][1]
+    if ret == 0:
+        return 9
+    else:
+        return ret - 1
+
+# condition checker for timestamp
+def lessThan(senderStamp, receiverStamp, senderIndex):
+    i = 0
+    while i < len(senderStamp):
+        # check for first condition
+        if i == senderIndex:
+            if senderStamp[i] != receiverStamp[i] + 1:
+                return False
+        else:
+            # check for second condition
+            if senderStamp[i] > receiverStamp[i]:
+                return False
+        i += 1
+    return True
 
 
 # build server for other nodes
@@ -47,13 +86,15 @@ def buildServer(port, num):
     sockForListen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     # create server
-    host = socket.gethostname();
+    host = socket.gethostname()
     sockForListen.bind((host, port))
     sockForListen.listen(num)
     print("server running....")
 
     # count number of connection
     count = 0
+
+    global holdBack
 
     while True:
         c, a = sockForListen.accept()
@@ -71,7 +112,6 @@ def buildServer(port, num):
 
 
 # handler for receiving messages from one node
-# TO DO: adding multicast & causal ordering here?
 def handler(c, a):
     global received
     # name of this node
@@ -80,7 +120,6 @@ def handler(c, a):
     # receiving messages from this node
     while True:
         data = c.recv(1024)
-        msg = str(data, 'utf-8')
 
         # failure detector using EOF
         if not data:
@@ -90,44 +129,51 @@ def handler(c, a):
             c.close()
             break
 
+        # deserialize the data
+        info= pickle.loads(data)
+
+        # add to hold-back queue
+        holdBack.append(info)
+
         # get the name from each node and store them
-        if "NAME&" in msg:
-            hostName = msg.split('&')[1]
+        if "NAME&" in info[1]:
+            hostName = info[1].split('&')[1]
             continue
+
+        # separate the info
+        sender_stamp = info[0]
+        msg          = info[1]
+        sender_index = info[2]
+
         # send the message
-        elif msg not in received:
+        if info not in received:
             # mutex to keep received thread-safe
             # add message to received
-            mutex.acquire()
-            received.append(msg)
-            mutex.release()
+            mutex_r.acquire()
+            received.append(info)
+            mutex_r.release()
 
             # B-multicast to any other nodes
-            sendMsg(msg)
+            sendMsg(info)
 
-            # avoid left bug
-            if msg != '':
-                print(msg)
-            # mutex to keep received thread-safe
-            # remove handled info from received
-        # mutex.acquire()
-        # received.remove(msg)
-        # mutex.release()
-
-
-
-# multicast group should be all the
-# clients on the current server thus
-# no need to keep track of multicast
-# group
-
-# message that one process has received
+        # wait until condition meets for hold-back
+        while True:
+            if :
+                # remove finished info from hold-back queue
+                holdBack.remove(info)
+                 # avoid left bug
+                if msg != '':
+                    print(msg)
+                timestamp[getN()] += 1;
+                
 
 
 # send message to all other nodes in the group
-def sendMsg(msg):
+def sendMsg(timestamp, msg, p_num):
     for sock in sockForSend:
-        sock.send(bytes(msg, 'utf-8'))
+        # serialize timestamp and message together 
+        info = pickle.dumps([timestamp, msg, p_num])
+        sock.send(info)
 
 
 # connect to other nodes' server using (n-1) sockets
@@ -167,20 +213,31 @@ def connectServer(port, num, name):
             client_checked = True
             break
 
+    # get the process number
+    p_num = getN()
+
     # send the name of this node to every server
     msg = "NAME&" + name
     for sock in sockForSend:
-        sock.send(bytes(msg, 'utf-8'))
+        info = pickle.dumps([timestamp, msg, p_num])
+        sock.send(info)
 
     # sending message to the socket
     while True:
         msg = name + ": " + input("")
         global received
         # append message to received when sender send it
-        mutex.acquire()
+        mutex_r.acquire()
         received.append(msg)
-        mutex.release()
-        sendMsg(msg)
+        mutex_r.release()
+
+        # update timestamp
+        global timestamp
+        timestamp[p_num] += 1
+
+        sendMsg(timestamp, msg, p_num)
+
+
         # for multicast test
         # sockForSend[0].send(bytes(msg, 'utf-8'))
 
@@ -195,6 +252,9 @@ def main():
     name = args.name
     port = args.port
     num  = args.number - 1
+
+    # initialize hold back queue
+    timestamp = [0] * (num + 1)
 
     server = threading.Thread(target=buildServer, args=(port, num))
     client = threading.Thread(target=connectServer, args=(port, num), kwargs={"name": name})
